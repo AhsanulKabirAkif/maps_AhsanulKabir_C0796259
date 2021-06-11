556d1812c30ac4e5b7be2246e2d3bdf8639d9611
Ahsanul Kabir <ahsanul297@gmail.com>
2021-06-10 19:15:49 -0900

    Little

--- app/src/main/java/com/example/maps_ahsanulkabir_c0796259/MapsActivity.java
+++ app/src/main/java/com/example/maps_ahsanulkabir_c0796259/MapsActivity.java
@@ -53,7 +53,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     private static final int POLYGON_SIDES = 4;
     List<Marker> markers = new ArrayList();
 
-    // location with location manager and listener
+
     LocationManager locationManager;
     LocationListener locationListener;
     Location location;
@@ -70,15 +70,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mapFragment.getMapAsync(this);
     }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
@@ -116,9 +107,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap.setOnMapLongClickListener(new GoogleMap.OnMapLongClickListener() {
             @Override
             public void onMapLongClick(LatLng latLng) {
-//                Location location = new Location("Your Destination");
-//                location.setLatitude(latLng.latitude);
-//                location.setLongitude(latLng.longitude);
                 // set marker
                 setMarker(latLng);
             }
@@ -128,14 +116,12 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 if(count == 1) {
                     MarkerOptions options = new MarkerOptions().position(latLng).title("Your Destination").snippet("A");
 
-                    // check if there are already the same number of markers, we clear the map.
                     if (markers.size() == POLYGON_SIDES)
                         clearMap();
 
                     markers.add(mMap.addMarker(options));
                     if (markers.size() == POLYGON_SIDES)
                         drawShape();
-
 
                 }
                 if(count == 2) {
@@ -205,8 +191,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
                 Geocoder geocoder = new Geocoder(MapsActivity.this);
 
-                Double markerLon = marker.getPosition().longitude;
-                Double markerLang = marker.getPosition().latitude;
+                double markerLon = marker.getPosition().longitude;
+                double markerLang = marker.getPosition().latitude;
 
                 try {
                     List<Address> addressList = geocoder.getFromLocation(markerLang, markerLon, 1);
@@ -223,14 +209,10 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
-
-
-
                 return false;
             }
         });
     }
-
 
     private void startUpdateLocation() {
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
